5e6a48c30153120b1e2403b601757e9a02b7a4ef
tea-tea-wyr <1623785079@qq.com>
2020-08-25 21:41:47 +0800

    1

---
+++ StudentCommunityApp/app/src/main/java/com/example/studentcommunityapp/util/WebHelper.java
@@ -0,0 +1,57 @@
+package com.example.studentcommunityapp.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class WebHelper {
+    //必须在子线程运行，非子线程运行会报错
+    public static MediaType jsonmediaType = MediaType.parse("application/json");
+    private static WebHelper webHelper;
+    private WebHelper(){}
+    public OkHttpClient client = new OkHttpClient();
+    public static synchronized WebHelper getInstance(){
+        if(webHelper==null)webHelper=new WebHelper();
+        return webHelper;
+    }
+    public static String getInfo(String url) throws IOException {
+        Request request=new Request.Builder().url(url).build();
+        Response response= WebHelper.getInstance().client.newCall(request).execute();
+        return response.body().string();
+    }
+    public static String getInfoWithCookie(String url,String cookie) throws IOException {
+        Request request=new Request.Builder().url(url).header("Cookie",cookie).build();
+        Response response= WebHelper.getInstance().client.newCall(request).execute();
+        return response.body().string();
+    }
+    public static String postInfo(String url, RequestBody body) throws  IOException{
+        Request request=new Request.Builder().url(url).post(body).build();
+        Response response= WebHelper.getInstance().client.newCall(request).execute();
+        return response.body().string();
+    }
+    public static String postWithCookie(String url, RequestBody body,String cookie) throws IOException {
+        Request request=new Request.Builder().url(url).header("Cookie",cookie).post(body).build();
+        Response response= WebHelper.getInstance().client.newCall(request).execute();
+        return response.body().string();
+    }
+    public static RequestBody SetJSonBody(JSONObject object){
+        return RequestBody.create(jsonmediaType,object.toString());
+    }
+    public static String getCookie(Context context){
+        SharedPreferences Infos = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+        return Infos.getString("cookie","");
+    }
+    public static void setCookie(Context context,String cookie){
+        SharedPreferences Infos = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+        Infos.edit().putString("cookie",cookie).apply();
+    }
+}
